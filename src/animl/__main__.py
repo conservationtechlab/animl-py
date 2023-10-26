f8cb1c36861496f14b38769f3055b3c283de97a3
Kyra Swanson <t.kyraswanson@gmail.com>
2023-10-26 15:48:31 -0700

    fix main to mkdir models if dne

--- src/animl/__main__.py
+++ src/animl/__main__.py
@@ -88,11 +88,13 @@ parser.add_argument('class_list', type=str, nargs='?',
                     default='../models/southwest_v2_classes.txt')
 # Parse the command-line arguments
 args = parser.parse_args()
+if not os.path.isdir('../models/'):
+    os.mkdir('../models/')
 
 if not os.path.isfile(args.detector_file):
     prompt = "MegaDetector not found, would you like to download? y/n: "
     if input(prompt).lower() == "y":
-        wget.download('https://sandiegozoo.box.com/shared/static/xj3496ii5hxtomf0s38axb1agn5u9up8.pt',
+        wget.download('https://github.com/agentmorris/MegaDetector/releases/download/v5.0/md_v5a.0.0.pt',
                       out='../models/')
 
 if not os.path.isfile(args.classifier_file):
