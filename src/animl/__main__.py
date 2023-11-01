d68d5f4997504c196359e9cdd5ec02678e20e251
tkswanson <ts9137b@gmail.com>
2023-11-01 14:31:58 -0700

    add examples zip, fix model download path

--- src/animl/__main__.py
+++ src/animl/__main__.py
@@ -67,42 +67,49 @@ def main(image_dir, detector_file, classifier_file, class_list):
 
 # Create an argument parser
 parser = argparse.ArgumentParser(description='Folder locations for the main script')
-
+home = os.path.join(os.getcwd(),'/models/')
 # Create and parse arguements
 parser.add_argument('image_dir', type=str,
                     help='Path to Image Directory')
 parser.add_argument('detector_file', type=str, nargs='?',
                     help='Path to MD model',
-                    default='../models/md_v5a.0.0.pt')
+                    default=os.path.join(home,'md_v5a.0.0.pt'))
 parser.add_argument('classifier_file', type=str, nargs='?',
                     help='Path to Class model',
-                    default='../models/southwest_v2.h5')
+                    default=os.path.join(home,'southwest_v2.h5'))
 parser.add_argument('class_list', type=str, nargs='?',
                     help='Path to class list',
-                    default='../models/southwest_v2_classes.txt')
+                    default=os.path.join(home,'/models/southwest_v2_classes.txt'))
 # Parse the command-line arguments
 
 args = parser.parse_args()
-if not os.path.isdir('../models/'):
-    os.mkdir('../models/')
 
 if not os.path.isfile(args.detector_file):
     prompt = "MegaDetector not found, would you like to download? y/n: "
     if input(prompt).lower() == "y":
+        if not os.path.isdir(home):
+            os.mkdir(home)
+        print('Saving to', home)
         wget.download('https://github.com/agentmorris/MegaDetector/releases/download/v5.0/md_v5a.0.0.pt',
-                      out='../models/')
+                      out= home)
 
 if not os.path.isfile(args.classifier_file):
     prompt = "Classifier not found, would you like to download Southwest_v2? y/n: "
     if input(prompt).lower() == "y":
+        if not os.path.isdir(home):
+            os.mkdir(home)
+        print('Saving to', home)
         wget.download('https://sandiegozoo.box.com/shared/static/x63lnaxw8hag39mczeommqy9tw4t0ht9.h5',
-                      out='../models/')
+                      out=home)
 
 if not os.path.isfile(args.class_list):
     prompt = "Class list not found, would you like to download Southwest_v2? y/n: "
     if input(prompt).lower() == "y":
+        if not os.path.isdir(home):
+            os.mkdir(home)
+        print('Saving to', home)
         wget.download('https://sandiegozoo.box.com/shared/static/hn8nput5pxjc3toao57gfn4h6zo1lyng.txt',
-                      out='../models/')
+                      out=home)
 
 # Call the main function
 main(args.image_dir, args.detector_file, args.classifier_file, args.class_list)
