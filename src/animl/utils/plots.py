6a3983af0495443ce765f5c4f73c92bd49c5ce9d
unduwapk <ukandage@ucsd.edu>
2023-11-15 11:53:35 -0800

    flake8 edits to plots.py

--- src/animl/utils/plots.py
+++ src/animl/utils/plots.py
@@ -19,7 +19,7 @@ import torch
 from PIL import Image, ImageDraw, ImageFont
 
 from .general import (CONFIG_DIR, FONT, LOGGER, Timeout, check_font, check_requirements, clip_coords,
-                           increment_path, is_ascii, threaded, try_except, xywh2xyxy, xyxy2xywh)
+                      increment_path, is_ascii, threaded, try_except, xywh2xyxy, xyxy2xywh)
 from .metrics import fitness
 
 # Settings
