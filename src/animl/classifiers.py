d07b54bd298d8b1119c9ede637ed99acb29bcc29
Kyra Swanson <t.kyraswanson@gmail.com>
2023-12-26 11:03:36 -0800

    flake8 classifiers

--- src/animl/classifiers.py
+++ src/animl/classifiers.py
@@ -3,12 +3,13 @@ from torch import flatten
 from torchvision.models import resnet
 from torchvision.models import efficientnet
 
+
 class CTLClassifier(nn.Module):
 
     def __init__(self, num_classes):
         '''
             Constructor of the model. Here, we initialize the model's
-            architecture (layers).
+            architecture (layers). NOT IN USE
         '''
         super(CTLClassifier, self).__init__()
 
@@ -46,27 +47,21 @@ class EfficientNet(nn.Module):
         super(EfficientNet, self).__init__()
         self.avgpool = nn.AdaptiveAvgPool2d(1)
 
-        self.model = efficientnet.efficientnet_v2_m(weights = efficientnet.EfficientNet_V2_M_Weights)       # "pretrained": use weights pre-trained on ImageNet
+        self.model = efficientnet.efficientnet_v2_m(weights=efficientnet.EfficientNet_V2_M_Weights)       # "pretrained": use weights pre-trained on ImageNet
         if tune:
             for params in self.model.parameters():
                 params.requires_grad = True
-        
-        num_ftrs = self.model.classifier[1].in_features
-        
-        self.model.classifier[1] = nn.Linear(in_features=num_ftrs, out_features=num_classes)
 
-    
+        num_ftrs = self.model.classifier[1].in_features
+
+        self.model.classifier[1] = nn.Linear(in_features=num_ftrs, out_features=num_classes)
 
     def forward(self, x):
         '''
-            Forward pass. Here, we define how to apply our model. It's basically
-            applying our modified ResNet-18 on the input tensor ("x") and then
-            apply the final classifier layer on the ResNet-18 output to get our
-            num_classes prediction.
+            Forward pass.
         '''
         # x.size(): [B x 3 x W x H]
-        x = self.model.features(x)    # features.size(): [B x 3 x W x H]
-
+        x = self.model.features(x)
         x = self.avgpool(x)
         x = flatten(x, 1)
 
