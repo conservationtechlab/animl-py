dbab9a77847b9ca52886cfa281c048031b934c2f
Kyra Swanson <t.kyraswanson@gmail.com>
2024-01-03 13:21:34 -0800

    flake8 new functions

--- src/animl/animl_results_to_md_results.py
+++ src/animl/animl_results_to_md_results.py
@@ -1,4 +1,4 @@
-########
+'''
 #
 # animl_results_to_md_results.py
 #
@@ -19,52 +19,44 @@
 #
 # Credit goes to Dan Morris https://github.com/agentmorris/MegaDetector/tree/main
 # Adding a modified script to animl-py repo
-########
-
-#%% Imports and constants
-
+'''
 import pandas as pd
 import json
-
-# It's a little icky to hard-code this here rather than importing from elsewhere
-# in the MD repo, but it seemed silly to take a dependency on lots of MD code
-# just for this, so, hard-coding.
-detection_category_id_to_name = {'0':'empty','1':'animal','2':'person','3':'vehicle'}
+import sys
+import argparse
 
 
-#%% Main function
+detection_category_id_to_name = {'0': 'empty', '1': 'animal', '2': 'person', '3': 'vehicle'}
 
-def animl_results_to_md_results(input_file,output_file=None):
+
+def animl_results_to_md_results(input_file, output_file=None):
     """
     Converts the .csv file [input_file] to the MD-formatted .json file [output_file].
-    
+
     If [output_file] is None, '.json' will be appended to the input file.
     """
-    
-    #%%
-    
     if output_file is None:
         output_file = input_file + '.json'
 
     df = pd.read_csv(input_file)
-    
-    expected_columns = ('file','category','detection_conf',
-                        'bbox1','bbox2','bbox3','bbox4','class','classification_conf')
-    
+
+    expected_columns = ('file', 'category', 'detection_conf',
+                        'bbox1', 'bbox2', 'bbox3', 'bbox4',
+                        'class', 'classification_conf')
+
     for s in expected_columns:
         assert s in df.columns,\
             'Expected column {} not found'.format(s)
-            
-    classification_category_name_to_id = {}    
+
+    classification_category_name_to_id = {}
     filename_to_results = {}
-    
-    # i_row = 0; row = df.iloc[i_row]
-    for i_row,row in df.iterrows():
+
+    for i_row, row in df.iterrows():
 
         if str(row['category']) == '0':
             continue
-        
-        # Is this the first detection we've seen for this file?            
+
+        # Is this the first detection we've seen for this file?
         if row['file'] in filename_to_results:
             im = filename_to_results[row['file']]
         else:
@@ -72,23 +64,19 @@ def animl_results_to_md_results(input_file,output_file=None):
             im['detections'] = []
             im['file'] = row['file']
             filename_to_results[im['file']] = im
-            
-        assert isinstance(row['category'],int),'Invalid category identifier in row {}'.format(im['file'])
+
+        assert isinstance(row['category'], int), 'Invalid category identifier in row {}'.format(im['file'])
         detection_category_id = str(row['category'])
         assert detection_category_id in detection_category_id_to_name,\
             'Unrecognized detection category ID {}'.format(detection_category_id)
-            
-        # Skip when empty image
-        #if detection_category_id=='0':
-            #continue
-        
+
         detection = {}
         detection['category'] = detection_category_id
         detection['conf'] = row['detection_conf']
-        bbox = [row['bbox1'],row['bbox2'],row['bbox3'],row['bbox4']]
+        bbox = [row['bbox1'], row['bbox2'], row['bbox3'], row['bbox4']]
         detection['bbox'] = bbox
         classification_category_name = row['class']
-        
+
         # Have we seen this classification category before?
         if classification_category_name in classification_category_name_to_id:
             classification_category_id = \
@@ -97,72 +85,54 @@ def animl_results_to_md_results(input_file,output_file=None):
             classification_category_id = str(len(classification_category_name_to_id))
             classification_category_name_to_id[classification_category_name] = \
                 classification_category_id
-        
-        classifications = [[classification_category_id,row['classification_conf']]]
+
+        classifications = [[classification_category_id, row['classification_conf']]]
         detection['classifications'] = classifications
-        
+
         im['detections'].append(detection)
-        
-    # ...for each row        
-        
+
+    # ...for each row
+
     info = {}
     info['format_version'] = '1.3'
     info['detector'] = 'Animl'
     info['classifier'] = 'Animl'
-    
+
     results = {}
     results['info'] = info
     results['detection_categories'] = detection_category_id_to_name
     results['classification_categories'] = \
         {v: k for k, v in classification_category_name_to_id.items()}
     results['images'] = list(filename_to_results.values())
-    
-    with open(output_file,'w') as f:
-        json.dump(results,f,indent=1)
-        
-# ...zamba_results_to_md_results(...)
-        
 
-#%% Interactive driver
+    with open(output_file, 'w') as f:
+        json.dump(results, f, indent=1)
 
-if False:
-
-    pass
-
-    #%%
-    
-    input_file = r"G:\temp\animl-runs\animl-runs\Coati_v2\manifest.csv"
-    output_file = None
-    animl_results_to_md_results(input_file,output_file)
-    
-
-#%% Command-line driver
-
-import sys,argparse
 
 def main():
 
     parser = argparse.ArgumentParser(
         description='Convert an Animl-formatted .csv results file to MD-formatted .json results file')
-    
+
     parser.add_argument(
         'input_file',
         type=str,
         help='input .csv file')
-    
+
     parser.add_argument(
         '--output_file',
         type=str,
         default=None,
         help='output .json file (defaults to input file appened with ".json")')
-    
+
     if len(sys.argv[1:]) == 0:
         parser.print_help()
         parser.exit()
 
     args = parser.parse_args()
 
-    animl_results_to_md_results(args.input_file,args.output_file)
-    
+    animl_results_to_md_results(args.input_file, args.output_file)
+
+
 if __name__ == '__main__':
     main()
