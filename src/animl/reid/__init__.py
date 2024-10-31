c2ea84f480d8cf4e47aa20303c43077789ced4f2
Kyra Swanson <t.kyraswanson@gmail.com>
2024-10-31 13:25:21 -0700

    fix miewid for mp

--- src/animl/reid/__init__.py
+++ src/animl/reid/__init__.py
@@ -7,13 +7,12 @@ from animl.reid.heads import (ArcFaceLossAdaptiveMargin,
                               ArcMarginProduct_subcenter, ElasticArcFace,
                               l2_norm,)
 from animl.reid.miewid import (GeM, IMAGE_HEIGHT, IMAGE_WIDTH, MiewIdNet, load,
-                               matchypatchy, weights_init_classifier,
-                               weights_init_kaiming,)
+                               weights_init_classifier, weights_init_kaiming,)
 from animl.reid.viewpoint import (IMAGE_HEIGHT, IMAGE_WIDTH, ViewpointModel,
-                                  load, matchypatchy,)
+                                  load,)
 
 __all__ = ['ArcFaceLossAdaptiveMargin', 'ArcFaceSubCenterDynamic',
            'ArcMarginProduct', 'ArcMarginProduct_subcenter', 'ElasticArcFace',
            'GeM', 'IMAGE_HEIGHT', 'IMAGE_WIDTH', 'MiewIdNet', 'ViewpointModel',
-           'heads', 'l2_norm', 'load', 'matchypatchy', 'miewid', 'viewpoint',
+           'heads', 'l2_norm', 'load', 'miewid', 'viewpoint',
            'weights_init_classifier', 'weights_init_kaiming']
