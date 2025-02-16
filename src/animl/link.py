ec5ff346da4887c901501ae61513ce633bd318d3
Kyra Swanson <t.kyraswanson@gmail.com>
2025-02-16 05:54:03 -0800

    same for md link

--- src/animl/link.py
+++ src/animl/link.py
@@ -101,9 +101,9 @@ def sort_MD(manifest: DataFrame,
         manifest.loc[i, 'Link'] = str(link)
 
         if copy:  # make a hard copy
-            copy2(row[file_col], link)
+            copy2(Path(row[file_col]), link)
         else:  # make a hard link
-            os.link(row[file_col], link)
+            os.link(Path(row[file_col]), link)
 
     return manifest
 
