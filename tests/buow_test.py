0a22f665fb894cbbc87f72473b5c5dba1b00f78a
Kyra Swanson <t.kyraswanson@gmail.com>
2025-08-14 13:21:32 -0700

    flake8

--- tests/buow_test.py
+++ tests/buow_test.py
@@ -1,10 +1,9 @@
 import time
-start_time = time.time()
-import shutil
 from pathlib import Path
-import pandas as pd
 
 from animl import file_management, detection, visualization
+
+start_time = time.time()
 
 print(Path.cwd())
 image_dir = Path.cwd() / 'examples' / 'BUOW'
@@ -13,18 +12,18 @@ detector = Path.cwd() / 'models/mani_buow_2025.pt'
 # get files
 files = file_management.build_file_manifest(image_dir, exif=False)
 
-
 detector = detection.load_detector(detector, "YOLO")
 
 md_results = detection.detect(detector,
                               files,
-                            file_col="Frame",
-                            batch_size=4,
-                            num_workers=4, confidence_threshold=0.5)
+                              file_col="Frame",
+                              batch_size=4,
+                              num_workers=4,
+                              confidence_threshold=0.5)
 
 detections = detection.parse_detections(md_results, manifest=files)
 
 print(detections)
 
-#Plot boxes
+# Plot boxes
 visualization.plot_all_bounding_boxes(detections, 'test/', file_col='Frame', prediction=False)
