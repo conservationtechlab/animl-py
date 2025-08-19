e076834e74af5efe7351c4b4e2f15a404e27e63c
Kyra Swanson <t.kyraswanson@gmail.com>
2025-08-19 16:11:07 -0700

    add folder create/delete for training test

--- tests/model_training_test.py
+++ tests/model_training_test.py
@@ -3,6 +3,8 @@ Test model training pipeline
 
 """
 import unittest
+import yaml
+import shutil
 from pathlib import Path
 
 from animl import train, test
@@ -12,6 +14,13 @@ from animl import train, test
 def model_training_test():
     print(Path.cwd())
     config = Path.cwd() / 'examples/Cats_vs_Dogs' / 'cat_dog_train.yml'
+
+    cfg = yaml.safe_load(open(config, 'r'))
+
+    output = Path(cfg['experiment_folder'])
+    if output.exists():
+        shutil.rmtree(output)
+    Path.mkdir(output)
 
     train.train_main(config)
     test.test_main(config)
