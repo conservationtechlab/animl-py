f203db7ee69fdf7dee64a71051e53357e8c11bc6
Kyra Swanson <t.kyraswanson@gmail.com>
2025-11-06 15:15:11 -0800

    fix onnx output for yolo

--- tests/onnx_export_test.py
+++ tests/onnx_export_test.py
@@ -13,24 +13,24 @@ What it does:
 """
 import unittest
 import torch
+import inspect
 import json
 import onnxruntime as ort
+import ultralytics
 
-from animl import load_classifier, load_class_list
+from animl import load_classifier, load_class_list, load_detector
 
-@unittest.skip
-def export_model(
-    model: str,
-    output_path: str,
-    input_names=("input",),
-    output_names=("output",),
-    opset_version: int = 13,
-    do_constant_folding: bool = True,
-    dynamic_batch_dim: bool = True,
-):
+def export_model(model: str,
+                 output_path: str,
+                 input_names=("input",),
+                 output_names=("output",),
+                 opset_version: int = 13,
+                 resize_height: int = 299,
+                 resize_width: int = 299,
+                 do_constant_folding: bool = True,
+                 dynamic_batch_dim: bool = True,):
 
-    example_input = torch.randn(1, 3, 299, 299, dtype=torch.float32)
-
+    example_input = torch.randn(1, 3, resize_height, resize_width, dtype=torch.float32)
     model.eval()
     model_cpu = model.to("cpu")
 
@@ -58,7 +58,14 @@ def export_model(
     )
     print("Export finished.")
 
-@unittest.skip
+
+def export_onnx(model_path: str, img_size: int = 1280):
+    model = ultralytics.YOLO(model_path)
+
+    # Export the model to ONNX format
+    model.export(format="onnx", imgsz=img_size, opset=13, dynamic=False, nms=True)
+
+
 def add_class_dict(model_path, class_dict):
     # Add class dictionary as metadata to the ONNX model
     import onnx
@@ -71,8 +78,7 @@ def add_class_dict(model_path, class_dict):
     print("Added class_dict metadata to ONNX model.")
 
 
-@unittest.skip
-def verify_with_onnxruntime(onnx_path: str, input_name: str = "input", device: str = "cpu"):
+def verify_with_onnxruntime(onnx_path: str, input_size):
     # Load ONNX model and show input shapes
 
     sess = ort.InferenceSession(onnx_path, providers=["CPUExecutionProvider"])
@@ -87,12 +93,12 @@ def verify_with_onnxruntime(onnx_path: str, input_name: str = "input", device: s
     for bs in (1, 4):
         # Construct random input matching the static non-batch dims in the model (first input shape)
         # Replace this creation with the proper shape and dtype for your model if using something else.
-        input_shape = [bs] + [int(d) if isinstance(d, int) else 299 for d in inp.shape[1:]]
+        input_shape = [bs] + [int(d) if isinstance(d, int) else input_size for d in inp.shape[1:]]
         x = np.random.randn(*input_shape).astype(np.float32)
         print(f"Running ONNX model with batch size = {bs}, input shape = {x.shape}")
-        outputs = sess.run(None, {inp.name: x})
+        outputs = sess.run(None, {inp.name: x})[0]
         print(outputs)
-        print("Got outputs:", [o.shape for o in outputs])
+        print(f"Output shape: {outputs.shape}")
 
     props = sess.get_modelmeta().custom_metadata_map
     if "class_dict" in props:
@@ -102,7 +108,6 @@ def verify_with_onnxruntime(onnx_path: str, input_name: str = "input", device: s
         print("No class_dict metadata found in ONNX model.")
 
     
-@unittest.skip
 def test_env_print():
     import sys, os
     print("PYTHON_EXECUTABLE:", sys.executable)
@@ -110,23 +115,29 @@ def test_env_print():
     print("PATH:", os.environ.get("PATH"))
     assert True
 
-
-@unittest.skip
+# @unittest.skip
 def main():
-    #test_env_print()
-    classes = load_class_list("models/sdzwa_southwest_v3_classes.csv")
+    test_env_print()
 
-    class_dict = {i: c['class'] for i, c in classes.iterrows()}
-    print(class_dict)
+    print("Signature:", inspect.signature(ultralytics.YOLO.export))
+    print("Docstring:\n", ultralytics.YOLO.export.__doc__)
 
-    model = load_classifier("models/sdzwa_southwest_v3.pt", len(class_dict))
 
-    export_model(model, "models/sdzwa_southwest_v3.onnx")
+    #megadetector
+    export_onnx("models/md_v1000.0.0-sorrel.pt", img_size=960)
+    verify_with_onnxruntime("models/md_v1000.0.0-sorrel.onnx", 960)
+
+   # classes = load_class_list("models/sdzwa_southwest_v3_classes.csv")
+   # class_dict = {i: c['class'] for i, c in classes.iterrows()}
+
+    #model = load_classifier("models/sdzwa_southwest_v3.pt", len(class_dict))
+
+#    export_model(model, "models/sdzwa_southwest_v3.onnx")
 
     # add class dict metadata
-    add_class_dict("models/sdzwa_southwest_v3.onnx", class_dict)
+ #   add_class_dict("models/sdzwa_southwest_v3.onnx", class_dict)
+  #  verify_with_onnxruntime("models/sdzwa_southwest_v3.onnx")
 
 
-    verify_with_onnxruntime("models/sdzwa_southwest_v3.onnx")
-
+# run test
 main()
