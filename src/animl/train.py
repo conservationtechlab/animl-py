88f0acb1bf39abc4ce29b80ca08005690815bcd1
Kyra Swanson <t.kyraswanson@gmail.com>
2026-07-30 15:40:51 -0700

    remove start_epoch from load_classifier and unpack predictions_output

--- src/animl/train.py
+++ src/animl/train.py
@@ -344,8 +344,8 @@ def train_classifier(cfg):
     mixed_precision = cfg.get('mixed_precision', False)
 
     # model will be on CPU after this call if cfg['experiment_folder'] is a directory
-    model, classes, current_epoch = load_classifier(cfg['experiment_folder'], cfg['class_file'],
-                                                    device=device, architecture=cfg['architecture'])
+    model, classes = load_classifier(cfg['experiment_folder'], cfg['class_file'],
+                                     device=device, architecture=cfg['architecture'])
 
     # Move model to the target device BEFORE optimizer initialization
     model.to(device)
