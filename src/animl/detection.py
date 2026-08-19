8279e363908e7a65c66051c52cc9eef817669080
Kyra Swanson <t.kyraswanson@gmail.com>
2026-08-19 16:06:19 -0700

    expand manifest_dataloarder args for legibility

--- src/animl/detection.py
+++ src/animl/detection.py
@@ -213,9 +213,12 @@ def detect(detector,
         device = get_torch_device(user_set=device, quiet=True)
 
     # create dataloader
-    dataloader = manifest_dataloader(manifest, file_col=file_col, batch_size=batch_size,
-                                     num_workers=num_workers, crop=False,
-                                     normalize=True, letterbox=letterbox,
+    dataloader = manifest_dataloader(manifest, 
+                                     batch_size=batch_size,
+                                     num_workers=num_workers, 
+                                     crop=False,
+                                     normalize=True, 
+                                     letterbox=letterbox,
                                      file_col=file_col,
                                      resize_width=resize_width,
                                      resize_height=resize_height)
