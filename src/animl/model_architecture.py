082c92e7c5c0d83f58e00091757133ba21f57713
Kyra Swanson <t.kyraswanson@gmail.com>
2026-09-16 16:25:16 -0700

    restructure reid

--- src/animl/model_architecture.py
+++ src/animl/model_architecture.py
@@ -7,14 +7,19 @@ import open_clip
 from peft import LoraConfig, get_peft_model
 import torch
 import torch.nn as nn
+import timm
+import torch.nn.functional as F
+import torch.nn as nn
 from torchvision.models import efficientnet, convnext_base, ConvNeXt_Base_Weights
 
 MEGADETECTORv5_SIZE = 1280
 MEGADETECTORv5_STRIDE = 64
 MD_LABELS = {0: "empty", 1: "animal", 2: "human",  3: "vehicle"}
 MD_MODELS = {"mdv5", "mdv6", "mdv1000-redwood", "mdv1000-spruce", "mdv1000-cedar", "mdv1000-larch", "mdv1000-sorrel"}
+
 SDZWA_CLASSIFIER_SIZE = 480
 BIOCLIP_CLASSIFIER_SIZE = 224
+MIEWID_SIZE = 440
 
 
 class EfficientNet(nn.Module):
@@ -110,3 +115,119 @@ class BioCLIP(nn.Module):
         features = features / features.norm(dim=-1, keepdim=True)
         logits = self.classifier(features)
         return logits
+
+
+class GeM(nn.Module):
+    def __init__(self, p: int = 3, eps: float = 1e-6) -> None:
+        super(GeM, self).__init__()
+        self.p = nn.Parameter(torch.ones(1)*p)
+        self.eps = eps
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.gem(x, p=self.p, eps=self.eps)
+
+    def gem(self, x: torch.Tensor, p: torch.Tensor, eps: float) -> torch.Tensor:
+        return F.avg_pool2d(x.clamp(min=eps).pow(p), (x.size(-2), x.size(-1))).pow(1./p)
+
+    def __repr__(self) -> str:
+        return self.__class__.__name__ + \
+                '(' + 'p=' + '{:.4f}'.format(self.p.data.tolist()[0]) + \
+                ', ' + 'eps=' + str(self.eps) + ')'
+
+
+class MiewIdNet(nn.Module):
+    def __init__(self,
+                 device=None,
+                 n_classes=10,
+                 model_name='efficientnetv2_rw_m',
+                 use_fc=False,
+                 fc_dim=512,
+                 dropout=0.0,
+                 loss_module='softmax',
+                 pretrained=True):
+
+        super(MiewIdNet, self).__init__()
+        print('Building Model Backbone for {} model'.format(model_name))
+
+        self.model_name = model_name
+        self.device = device
+
+        self.backbone = timm.create_model(model_name, pretrained=pretrained)
+        if model_name.startswith('efficientnetv2_rw'):
+            final_in_features = self.backbone.classifier.in_features
+        if model_name.startswith('swinv2'):
+            final_in_features = self.backbone.norm.normalized_shape[0]
+
+        self.backbone.classifier = nn.Identity()
+        self.backbone.global_pool = nn.Identity()
+
+        self.pooling = GeM()
+        self.bn = nn.BatchNorm1d(final_in_features)
+        self.use_fc = use_fc
+        if use_fc:
+            self.dropout = nn.Dropout(p=dropout)
+            self.bn = nn.BatchNorm1d(fc_dim)
+            self.bn.bias.requires_grad_(False)
+            self.fc = nn.Linear(final_in_features, n_classes, bias=False)
+            self.bn.apply(self.weights_init_kaiming)
+            self.fc.apply(self.weights_init_classifier)
+            final_in_features = fc_dim
+
+        self.loss_module = loss_module
+        self.final = nn.Linear(final_in_features, n_classes)
+
+    def _init_params(self) -> None:
+        nn.init.xavier_normal_(self.fc.weight)
+        nn.init.constant_(self.fc.bias, 0)
+        nn.init.constant_(self.bn.weight, 1)
+        nn.init.constant_(self.bn.bias, 0)
+
+    def forward(self, x: torch.Tensor, label: torch.Tensor = None) -> torch.Tensor:
+        feature = self.extract_feat(x)
+        return feature
+
+    def extract_feat(self, x: torch.Tensor) -> torch.Tensor:
+        batch_size = x.shape[0]
+        x = self.backbone.forward_features(x)
+        if self.model_name.startswith('swinv2'):
+            x = x.permute(0, 3, 1, 2)
+
+        x = self.pooling(x).view(batch_size, -1)
+        x = self.bn(x)
+        if self.use_fc:
+            x1 = self.dropout(x)
+            x1 = self.bn(x1)
+            x1 = self.fc(x1)
+
+        return x
+
+    def extract_logits(self, x: torch.Tensor, label: torch.Tensor = None) -> torch.Tensor:
+        feature = self.extract_feat(x)
+        assert label is not None
+        if self.loss_module in ('arcface', 'arcface_subcenter_dynamic'):
+            logits = self.final(feature, label)
+        else:
+            logits = self.final(feature)
+
+        return logits
+
+    def weights_init_kaiming(m):
+        classname = m.__class__.__name__
+        if classname.find('Linear') != -1:
+            nn.init.kaiming_normal_(m.weight, a=0, mode='fan_out')
+            nn.init.constant_(m.bias, 0.0)
+        elif classname.find('Conv') != -1:
+            nn.init.kaiming_normal_(m.weight, a=0, mode='fan_in')
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0.0)
+        elif classname.find('BatchNorm') != -1:
+            if m.affine:
+                nn.init.constant_(m.weight, 1.0)
+                nn.init.constant_(m.bias, 0.0)
+
+    def weights_init_classifier(m: nn.Module) -> None:
+        classname = m.__class__.__name__
+        if classname.find('Linear') != -1:
+            nn.init.normal_(m.weight, std=0.001)
+            if m.bias:
+                nn.init.constant_(m.bias, 0.0)
